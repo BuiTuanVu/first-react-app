de55dcab17d4ef833437a67280a57fa533b4534e
BuiTuanVu <vubui2010@gmail.com>
2019-09-11 18:57:15 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { firNumber: '', secNumber: '', sum: '' };
+
+    this.handleChangeFirstNumber = this.handleChangeFirstNumber.bind(this);
+    this.handleChangeSecondNumber = this.handleChangeSecondNumber.bind(this);
+    this.handleRefresh = this.handleRefresh.bind(this);
+  }
+
+
+
+  handleChangeFirstNumber(event) {
+    this.setState({ firNumber: event.target.value },
+      () => { this.setState({ sum: Number(this.state.firNumber) + Number(this.state.secNumber) }) }
+    );
+  }
+
+  handleChangeSecondNumber(event) {
+    this.setState({ secNumber: event.target.value },
+      () => { this.setState({ sum: Number(this.state.firNumber) + Number(this.state.secNumber) }) }
+    );
+
+  }
+
+  handleRefresh() {
+    this.setState({ firNumber: '' },
+      () => { this.setState({ secNumber: '' }, 
+        () => { this.setState( {sum: ''})}) }
+    );
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="card">
+          <h5 className="card-header">Sample Calculator</h5>
+          <div className="card-body">
+            <h5 className="card-title">1612825-BTCN01</h5>
+            <p className="card-text">Enter the two numbers and automatically display the sum of the entered numbers. </p>
+            <p className="card-text">You can use the bellow button to refresh the form.</p>
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-row">
+                <div className="col-3">
+                  <input type="text" className="form-control" placeholder="First Number" value={this.state.firNumber} onChange={this.handleChangeFirstNumber} onBlur={this.handleCheck}/>
+                </div>
+                <div className="col-1">
+                  <label>+</label>
+                </div>
+                <div className="col-3">
+                  <input type="text" className="form-control" placeholder="Second Number" value={this.state.secNumber} onChange={this.handleChangeSecondNumber} />
+                </div>
+                <div className="col-1">
+                  <label>=</label>
+                </div>
+                <div className="col-3">
+                  <input type="text" className="form-control" placeholder="Result" value={this.state.sum} />
+                </div>
+                <div className="col-1">
+                <button type="button" className="btn btn-success" onClick={this.handleRefresh}><i class="fas fa-sync-alt"></i></button>
+                </div>
+                
+              </div>
+            </form>
+          </div>
+        </div>
+
+
+
+      </div>
+    );
+  }
 }
+
+
+
 
 export default App;
