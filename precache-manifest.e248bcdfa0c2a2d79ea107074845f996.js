fa2a36f3e394b500af8be6320fda0b39b4b4daf0
BuiTuanVu <vubui2010@gmail.com>
2019-09-11 19:07:19 +0700

    Updates

---
+++ precache-manifest.e248bcdfa0c2a2d79ea107074845f996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3ab66979d16e41d7a409bb7cf4bca8",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "36c0cc240bf824066be6",
+    "url": "/first-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2c193e9f923133774bce",
+    "url": "/first-react-app/static/js/2.01446dfa.chunk.js"
+  },
+  {
+    "revision": "36c0cc240bf824066be6",
+    "url": "/first-react-app/static/js/main.c76c2d21.chunk.js"
+  },
+  {
+    "revision": "e452194d4d667fb4a9aa",
+    "url": "/first-react-app/static/js/runtime~main.ec286e7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
